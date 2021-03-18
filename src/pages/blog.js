965a9b0dbbd1dea963089a92547e28c27a19d941
Casey James Perno <casey@khaosophy.com>
2021-03-18 10:21:23 -0400

    no anonymous page exports in blog redirect

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import { navigate } from 'gatsby';
 
-export default () => {
+const BlogRedirect = () => {
   useEffect(() => {
     navigate('/');
   }, []);
   return null;
 };
+
+export default BlogRedirect;
