725c5d21aad652b1eb9667e45b5af473cbbfb57e
Casey James Perno <casey@khaosophy.com>
2022-07-11 12:25:21 -0400

    sitemap configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
 export default defineConfig({
   markdown: {
     syntaxHighlight: 'prism',
   },
+  site: 'https://caseyjamesperno.com',
   integrations: [
     react(),
+    sitemap(),
   ],
 });
