ebef62398a6df6683772b6bda7bd52ef14e40a1c
Casey Perno <casey@khaosophy.com>
2023-09-08 11:24:00 -0400

    add support for mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   markdown: {
-    syntaxHighlight: 'prism',
+    syntaxHighlight: 'prism'
   },
   site: 'https://caseyjamesperno.com',
-  integrations: [
-    react(),
-    sitemap(),
-  ],
+  integrations: [react(), sitemap(), mdx()]
 });
