7a8a74a1ac3b37bd2d33f58a6ab8b929cfa6797f
Casey Perno <casey@khaosophy.com>
2023-11-16 16:21:38 -0500

    anchor link markup

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,25 @@ export default defineConfig({
     syntaxHighlight: 'prism',
     rehypePlugins: [
       rehypeSlug,
-      rehypeAutolinkHeadings,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'prepend',
+          content: {
+            type: 'element',
+            tagName: 'span',
+            properties: {
+              className: ['icon', 'icon-link'], // todo: what icon should we use / do we have access to?
+            },
+          },
+          headingProperties: {
+            className: ['anchor'],
+          },
+          properties: {
+            className: ['anchor-link'],
+          },
+        },
+      ],
     ]
   },
   site: 'https://caseyjamesperno.com',
